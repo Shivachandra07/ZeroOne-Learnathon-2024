aa9f737f94daab777f6fb14a7e12b8192c4b8d7c
Shivachandra07 <151779900+Shivachandra07@users.noreply.github.com>
2024-10-18 13:04:11 +0530

    2300032319_Biradar Shivachandra

---
+++ calc.js
@@ -0,0 +1,19 @@
+const add = (a,b)=>{
+    return a+b;
+}
+console.log(1,2);
+
+const mul = (a,b)=>{
+    return a*b;
+}
+console.log(2*4);
+
+const sub = (a,b)=>{
+    return a*b;
+} 
+console.log(4-3);
+
+const div = (a,b)=>{
+    return a/b;
+}
+console.log(4/8);
